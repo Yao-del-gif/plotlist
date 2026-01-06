407f4c820792e42a542178a99c7e61dd2609e9e7
Yao-del-gif <yaao7612@gmail.com>
2026-01-06 08:37:02 +0630

    second commit

---
+++ contact.js
@@ -0,0 +1,7 @@
+function openContactModal() {
+    document.getElementById('contactModal').style.display = 'flex';
+}
+
+function closeContactModal() {
+    document.getElementById('contactModal').style.display = 'none';
+}
